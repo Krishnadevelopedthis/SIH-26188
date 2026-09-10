74643c8c82d15be9241faff4d983a8b3509fc50b
Krishna Pandey <krishnapandey1296@gmail.com>
2026-09-10 20:16:08 +0530

    Add allowed origins for CORS in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,8 +15,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+         "https://airoease.live",
+        "https://www.airoease.live",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
